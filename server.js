969c7fc5635e8095b5c1c4a15d6af8f80cf4f918
Matt Zhao <mattzhao@MP.ht.home>
2023-05-17 18:10:34 -0300

    init 01

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
-const express = require('express')
+const express = require('express');
+const db = require('./config/connection');
+const { Reaction, Thought, User } = require('./models');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
